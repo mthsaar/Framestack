ce8502a65a151c39708e5553805ce798b3784841
Matheus Saar <matheussaar@gmail.com>
2026-04-25 19:36:46 -0300

    Projeto Framestack criado

---
+++ Framestack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Framestack
+//
+//  Created by Matheus  Saar on 25/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Framestack/FramestackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramestackApp.swift
+//  Framestack
+//
+//  Created by Matheus  Saar on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FramestackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
